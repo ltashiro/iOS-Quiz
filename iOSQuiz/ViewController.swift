33152a884cdb5a987cee438282c407d06480659f
Lilian <lil_tash38@hotmail.com>
2018-01-09 18:42:26 -0800

    Initial Commit

---
+++ iOSQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSQuiz
+//
+//  Created by Lilian Tashiro on 1/9/18.
+//  Copyright © 2018 Apple, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
